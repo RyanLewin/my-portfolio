87411becc1babb5aeac8ca83cf7bc5c48761498d
rlewin <ryanjlewin@gmail.com>
2026-07-31 08:41:42 +0100

    Added radix accordion and start of customisable content to add to accordion. Adjusted margins and padding for smaller screens.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,38 @@
 import Image from "next/image";
 import "./globals.css";
+import AccordionCustom from "@/components/AccordionCustom";
+
+const accordionSections = [
+  {
+    id: "about-me", 
+    triggerText: "About Me", 
+    contentText: (
+      <div>
+        <p className="mb-2">Computer Games Programming BSc graduate attaining an upper 2nd class honours looking for a programming position. I have lots of experience in C# and C++ with an ability to quickly pick up new languages and skills when necessary.</p> 
+        <p className="mb-2">After graduating I had a bit of time off to spend with family and worked on personal projects, trying to add to my portfolio a bit more, whilst looking out for any jobs that spark my interest. Then lockdown occurred and I got approached for a remote contract job at Poly Fruit Studios, formerly Pineapple Studios, as a Games Programmer, working for equity and then moving onto the Government Kickstarter scheme from February to August 2021. We had a lot of issues due to being an inexperienced team, but we successfully released a number of games and it taught me a lot about programming, source control and teamwork.</p> 
+        <p className="mb-2">I then did a few months of temporary work inputting data, before joining PsychoHound as a Games Programmer. Working on the official AUDL video game and our own IPs, Ruff ‘N’ Tumble and ‘FB’. The first two games are developed using UE4, and the latter using UE5 along with experience using Gameplay Ability Systems and PlayFab.</p> 
+        <p className="mb-2">Most recently, I have gained experience as a trainee computer science teacher, studying for my PGCE at the University of Huddersfield. However, I decided that it wasn’t what I wanted to do in life and so I’m now looking for work in a more programming/engineering role again whilst development personal projects in my free time.</p>
+      </div>
+    )
+  },
+  {
+    id: "past-experiences",
+    triggerText: "Past Experiences",
+    contentText: (
+      <div>
+        <p className="text-2xl">University of Huddersfield - Trainee Teacher</p>
+        <p><i>(Sep 2024 - Mar 2025)</i></p>
+        <p>I enrolled onto the Initial Teacher Training Course for the summer of 2024 specialising in Computer Science for Secondary school. However, after much deliberation and despite tutors and mentors telling me I had promise, I decided that teaching wasn’t a passion of mine and it was taking a toll on my mental health and so withdrew from the course.</p>
+      </div>
+    )
+  }
+]
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-background font-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-background sm:items-start">
-        <div className="flex flex-row items-center gap-8">
+      <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-3 lg:px-16 bg-background sm:items-center">
+        <div className="flex flex-col xl:flex-row  self-center items-center gap-8 mb-8">
           <Image
             className="h-48 w-48 object-[0%_40%] object-cover rounded-full  border-4 border-foreground"
             src="/GraduationPhoto.jpg"
@@ -15,10 +42,11 @@ export default function Home() {
             priority
           />
           <div className="flex flex-col">
-            <p className="text-7xl">Ryan Lewin</p>
-            <p className="text-5xl">Programmer</p>
+            <p className="text-7xl text-center xl:text-left">Ryan Lewin</p>
+            <p className="text-5xl text-center xl:text-left">Programmer</p>
           </div>
         </div>
+        <AccordionCustom items={accordionSections} className="w-full mb-8"></AccordionCustom>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-foreground dark:text-zinc-50">
             To get started, edit the page.tsx file.
---
+++ components/AccordionCustom.tsx
@@ -0,0 +1,96 @@
+import * as React from "react";
+import { Accordion } from "radix-ui";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { cn } from "./ui/utils";
+import "../app/styles/accordionstyle.css"
+
+export type AccordionItemData = {
+	id: string;
+	triggerText: string;
+	contentText: React.ReactNode; // Allows both raw strings and JSX elements
+};
+
+// 2. Define the props for the root custom component
+interface AccordionCustomProps {
+	items: AccordionItemData[];
+    className: string
+}
+
+const AccordionCustom = ({ className, items }: AccordionCustomProps) => (
+	<Accordion.Root 
+		className={cn("w-75 rounded-md bg-tint-bg shadow-[0_2px_10px] shadow-black/5", className)}
+		type="single"
+		defaultValue="item-1"
+		collapsible
+	>
+        {items.map((item) => (
+            <AccordionItem value={item.id} key={item.id} className="mb-2">
+                <AccordionTrigger>{item.triggerText}</AccordionTrigger>
+                <AccordionContent className="AccordionContent">{item.contentText}</AccordionContent>
+            </AccordionItem>
+
+        ))}
+	</Accordion.Root>
+);
+
+function AccordionItem({ 
+	children, 
+	className, 
+	...props 
+}: React.ComponentProps<typeof Accordion.Item>) {
+	return (
+		<Accordion.Item
+			className={cn(
+				"mt-px overflow-hidden first:mt-0 first:rounded-t last:rounded-b focus-within:relative focus-within:z-10 focus-within:shadow-[0_0_0_2px] focus-within:shadow-mauve12",
+				className,
+			)}
+			{...props}
+		>
+			{children}
+		</Accordion.Item>
+	);
+}
+
+function AccordionTrigger({ 
+	children, 
+	className, 
+	...props 
+}: React.ComponentProps<typeof Accordion.Trigger>) {
+	return (
+		<Accordion.Header className="flex">
+			<Accordion.Trigger
+				className={cn(
+					"group flex h-11.25 flex-1 cursor-default items-center justify-between bg-mauve1 px-5 text-[15px] leading-none text-violet11 shadow-[0_1px_0] shadow-mauve6 outline-none hover:bg-mauve2",
+					className,
+				)}
+				{...props}
+			>
+				{children}
+				<ChevronDownIcon
+					className="text-violet10 transition-transform duration-300 ease-[cubic-bezier(0.87,0,0.13,1)] group-data-[state=open]:rotate-180"
+					aria-hidden
+				/>
+			</Accordion.Trigger>
+		</Accordion.Header>
+	);
+}
+
+function AccordionContent({ 
+	children, 
+	className, 
+	...props 
+}: React.ComponentProps<typeof Accordion.Content>) {
+	return (
+		<Accordion.Content
+			className={cn(
+				"overflow-hidden bg-mauve2 text-[15px] text-mauve11 data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown",
+				className,
+			)}
+			{...props}
+		>
+			<div className="px-5 py-3.75">{children}</div>
+		</Accordion.Content>
+	);
+}
+
+export default AccordionCustom;
